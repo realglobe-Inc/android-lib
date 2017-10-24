c314f73a6b0254b78ce72e26f082d2cc8a03179c
fukuchidaisuke <fukuchidaisuke@realglobe.jp>
2017-10-24 11:52:51 +0900

    整理

--- lib/src/main/java/jp/realglobe/android/util/BaseActivity.java
+++ lib/src/main/java/jp/realglobe/android/util/BaseActivity.java
@@ -23,7 +23,6 @@ import android.os.Bundle;
 import android.os.Looper;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
@@ -83,26 +82,25 @@ public abstract class BaseActivity extends AppCompatActivity {
             if (onPermitted != null) {
                 onPermitted.run();
             }
-            return;
+        } else {
+            int rc = this.requestCode++;
+            this.requestCallbacks.put(rc, new Callback() {
+                @Override
+                public void onPermitted() {
+                    if (onPermitted != null) {
+                        onPermitted.run();
+                    }
+                }
+
+                @Override
+                public void onDenied(String[] denied) {
+                    if (onDenied != null) {
+                        onDenied.accept(denied);
+                    }
+                }
+            });
+            requestPermissions(permissions, rc);
         }
-
-        int rc = this.requestCode++;
-        this.requestCallbacks.put(rc, new Callback() {
-            @Override
-            public void onPermitted() {
-                if (onPermitted != null) {
-                    onPermitted.run();
-                }
-            }
-
-            @Override
-            public void onDenied(String[] denied) {
-                if (onDenied != null) {
-                    onDenied.accept(denied);
-                }
-            }
-        });
-        ActivityCompat.requestPermissions(this, permissions, rc);
     }
 
     @Override
@@ -128,9 +126,9 @@ public abstract class BaseActivity extends AppCompatActivity {
     }
 
     /**
-     * メッセージを画面に重ねて表示する。
+     * メッセージを画面に表示する。
      *
-     * @param message 表示するメッセージ
+     * @param message メッセージ
      */
     protected void showToast(@NonNull final String message) {
         if (Looper.myLooper() == Looper.getMainLooper()) {
